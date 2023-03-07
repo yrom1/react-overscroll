77e2a271a1d7f32ef1a06ce0d87ac0ae6f84b92a
Ryan Moore <ryanm.inbox@gmail.com>
2023-03-07 17:06:31 -0500

    simplier

--- bar/src/App.js
+++ bar/src/App.js
@@ -3,33 +3,23 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState('#fff');
+  const [background, setBackground] = useState('#000');
 
   useEffect(() => {
-    setColor('#fff'); // Set content background color to white
-    document.body.style.backgroundColor = '#000'; // Change body background color to black
-    document.body.style.color = '#fff'; // Change text color to white
-
-    // Add classes to body to create black overscroll at top and bottom
-    document.body.classList.add('overscroll-top');
-    document.body.classList.add('overscroll-bottom');
-
-    return () => {
-      // Clean up classes when component unmounts
-      document.body.classList.remove('overscroll-top');
-      document.body.classList.remove('overscroll-bottom');
-    };
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
   }, []);
 
   return (
     <div className="App">
-      <div className="Header" style={{ backgroundColor: '#000' }}>
+      <div className="Header" style={{ backgroundColor: background }}>
         <h2>This is the header.</h2>
       </div>
-      <div className="Content" style={{ backgroundColor: color, paddingTop: '100vh', color: '#000' }}>
+      <div className="Content" style={{ backgroundColor: color, paddingTop: '100vh', color: background }}>
         <h1>Hello, world!</h1>
         <p>The content background color is white, and there is black overscroll at the top and bottom of the page.</p>
       </div>
-      <div className="Footer" style={{ backgroundColor: '#000' }}>
+      <div className="Footer" style={{ backgroundColor: background }}>
         <h2>This is the footer.</h2>
       </div>
     </div>
